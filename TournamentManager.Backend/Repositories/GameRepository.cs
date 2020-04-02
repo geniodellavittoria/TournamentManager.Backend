0db6cbf470178429176f4425bd84f53ac927ca88
Tim Bolzern <tbo@bica.ch>
2020-04-02 11:44:25 +0200

    renamed to seperate from services

---
+++ TournamentManager.Backend/Repositories/GameRepository.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TournamentManager.Backend.Models;
+
+namespace TournamentManager.Backend.Services
+{
+    public class GameRepository
+    {
+        private readonly IMongoCollection<GroupGames> _games;
+
+        public GameRepository(ITournamentManagerDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _games = database.GetCollection<GroupGames>(settings.GamesCollectionName);
+        }
+
+        public async Task<ICollection<GroupGames>> Get() =>
+            await _games.Find(groupGames => true).ToListAsync();
+
+        public async Task<List<GroupGames>> GetGamesOfAGroup(string groupId) =>
+            await _games.Find(groupGame => groupGame.GroupId == groupId).ToListAsync();
+
+        public GroupGames Create(GroupGames groupGames)
+        {
+            _games.InsertOneAsync(groupGames);
+            return groupGames;
+        }
+    }
+}
+
---
+++ TournamentManager.Backend/Repositories/GroupRepository.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TournamentManager.Backend.Models;
+
+namespace TournamentManager.Backend.Services
+{
+    public class GroupRepository
+    {
+        private readonly IMongoCollection<Group> _groups;
+
+        public GroupRepository(ITournamentManagerDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _groups = database.GetCollection<Group>(settings.GroupsCollectionName);
+        }
+
+        public async Task<List<Group>> Get() =>
+            await _groups.Find(group => true).ToListAsync();
+
+        public async Task<Group> Get(string id)
+        {
+            return await _groups.Find(group => group.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Group Create(Group group)
+        {
+            _groups.InsertOneAsync(group);
+            return group;
+        }
+
+        public void Update(string id, Group groupIn) =>
+            _groups.ReplaceOneAsync(group => group.Id == id, groupIn);
+
+        public void Remove(string id) =>
+            _groups.DeleteOneAsync(group => group.Id == id);
+    }
+}
+
